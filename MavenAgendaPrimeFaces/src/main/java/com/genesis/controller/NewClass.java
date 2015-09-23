ab6e93e1aa6814559d19b44090fda44d0618ed81
MURCIA <MURCIA@murcia-desktop>
2015-09-23 17:34:03 -0600

    se agrego new class.java en paquete com.genesis.controller para que aparezca ese paquete en cualquier pc haciendo pull del sitio. luego se eliminará.

---
+++ MavenAgendaPrimeFaces/src/main/java/com/genesis/controller/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.genesis.controller;
+
+/**
+ *
+ * @author MURCIA
+ */
+public class NewClass {
+    
+}
